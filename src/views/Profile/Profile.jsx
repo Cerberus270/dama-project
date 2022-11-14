9bb82129806a317097db5bc8c4b83811884923be
RobertoMarquez19 <mercheroberto02@gmail.com>
2022-11-14 10:51:59 -0600

    Se cambio la paleta de colores de los botones

---
+++ src/views/Profile/Profile.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { NativeBaseProvider, ScrollView,Box } from 'native-base'
+
+export default function Profile(){
+    return (
+      <NativeBaseProvider>
+        <ScrollView>
+          <Box>
+
+          </Box>
+        </ScrollView>
+      </NativeBaseProvider>
+    )
+}
