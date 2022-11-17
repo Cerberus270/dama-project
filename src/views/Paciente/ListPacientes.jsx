77ac07ab7b389dff4019db5c0c9434b8545f2647
Cerberus270 <geoavila718@gmail.com>
2022-11-17 14:35:57 -0600

    Merge

--- src/views/Paciente/ListPacientes.jsx
+++ src/views/Paciente/ListPacientes.jsx
@@ -2,12 +2,12 @@ import { Text, View, ScrollView, StyleSheet, Alert } from "react-native";
 import React, { Component, useDebugValue, useEffect, useState } from "react";
 import { NativeBaseProvider, Heading, Box } from "native-base";
 import {
-  collection,
-  getDocs,
-  query,
-  where,
-  doc,
-  deleteDoc,
+    collection,
+    getDocs,
+    query,
+    where,
+    doc,
+    deleteDoc,
 } from "firebase/firestore";
 import { db, auth } from "../../../config/firebase";
 import { ListItem, Button, Avatar, Badge } from "react-native-elements";
@@ -16,202 +16,203 @@ import { useFocusEffect } from "@react-navigation/native";
 import { ActivityIndicator } from "react-native";
 
 export default function ListPacientes({ navigation }) {
-  const [pacientes, setPacientes] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [pacientes, setPacientes] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  const avatarPic = (typePaciente) => {
-    const avatar = {
-      Canino: require("../../../assets/avatars-tipo/perro.png"),
-      Felino: require("../../../assets/avatars-tipo/gato.png"),
-      Ave: require("../../../assets/avatars-tipo/aguila.png"),
-      Roedor: require("../../../assets/avatars-tipo/rata.png"),
-      Reptil: require("../../../assets/avatars-tipo/reptil.png"),
-      Anfibio: require("../../../assets/avatars-tipo/rana.png"),
-      Insecto: require("../../../assets/avatars-tipo/insecto.png"),
+    const avatarPic = (typePaciente) => {
+        const avatar = {
+            Canino: require("../../../assets/avatars-tipo/perro.png"),
+            Felino: require("../../../assets/avatars-tipo/gato.png"),
+            Ave: require("../../../assets/avatars-tipo/aguila.png"),
+            Roedor: require("../../../assets/avatars-tipo/rata.png"),
+            Reptil: require("../../../assets/avatars-tipo/reptil.png"),
+            Anfibio: require("../../../assets/avatars-tipo/rana.png"),
+            Insecto: require("../../../assets/avatars-tipo/insecto.png"),
+        };
+        return avatar[typePaciente];
     };
-    return avatar[typePaciente];
-  };
 
-  const actualizarEmpleado = (paciente) => {
-    // AQUI VA EL DE ACTUALIZAR
-    //navigation.navigate('DetailsPaciente');
-    console.log("Hola", paciente);
-  };
+    const actualizarEmpleado = (paciente) => {
+        // AQUI VA EL DE ACTUALIZAR
+        //navigation.navigate('DetailsPaciente');
+        console.log("Hola", paciente);
+    };
 
-  const getPacientes = async () => {
-    setLoading(true);
-    let listaPacientes = [];
-    const q = query(
-      collection(db, "patients"),
-      where("veterinario", "==", auth.currentUser.uid)
-    );
-    const pacientesDocs = await getDocs(q);
-    pacientesDocs.forEach((paciente) => {
-      const {
-        fechaNacimiento,
-        fechaRegistro,
-        nombre,
-        peso,
-        propietario,
-        raza,
-        sexo,
-        tipo,
-        veterinario,
-      } = paciente.data();
-      listaPacientes.push({
-        id: paciente.id,
-        fechaNacimiento,
-        fechaRegistro,
-        nombre,
-        peso,
-        propietario,
-        raza,
-        sexo,
-        tipo,
-        veterinario,
-      });
-    });
-    setLoading(false);
-    setPacientes(listaPacientes);
-    console.log(listaPacientes);
-  };
-
-  useFocusEffect(
-    React.useCallback(() => {
-      getPacientes();
-      return () => {
+    const getPacientes = async () => {
+        setLoading(true);
+        let listaPacientes = [];
+        const q = query(
+            collection(db, "patients"),
+            where("veterinario", "==", auth.currentUser.uid)
+        );
+        const pacientesDocs = await getDocs(q);
+        pacientesDocs.forEach((paciente) => {
+            const {
+                fechaNacimiento,
+                fechaRegistro,
+                nombre,
+                peso,
+                propietario,
+                raza,
+                sexo,
+                tipo,
+                veterinario,
+            } = paciente.data();
+            listaPacientes.push({
+                id: paciente.id,
+                fechaNacimiento,
+                fechaRegistro,
+                nombre,
+                peso,
+                propietario,
+                raza,
+                sexo,
+                tipo,
+                veterinario,
+            });
+        });
         setLoading(false);
-        setPacientes([]);
-      };
-    }, [])
-  );
+        setPacientes(listaPacientes);
+        console.log(listaPacientes);
+    };
 
-  return (
-    <NativeBaseProvider>
-      {loading ? (
-        <ActivityIndicator
-          style={styles.indicador}
-          size="large"
-          color="rgba(117, 140, 255, 1)"
-        />
-      ) : (
-        <ScrollView>
-          <Box m={5} mt={8} flex={1} p={1}>
-            <Heading
-              size="lg"
-              color="coolGray.800"
-              _dark={{
-                color: "warmGray.50",
-              }}
-              fontWeight="semibold"
-              alignSelf="center"
-            >
-              Gestion de Pacientes
-            </Heading>
-            <View style={styles.container}>
-              {pacientes.map((paciente) => {
-                return (
-                  <ListItem.Swipeable
-                    key={paciente.id}
-                    bottomDivider
-                    onPress={() => {
-                      navigation.navigate("TabPaciente", paciente);
-                    }}
-                    rightContent={
-                      <Button
-                        title="Actualizar"
-                        onPress={() => {
-                          actualizarEmpleado(paciente);
-                        }}
-                        icon={{ name: "info", color: "white" }}
-                        buttonStyle={{ minHeight: "100%" }}
-                      />
-                    }
-                  >
-                    <Avatar source={avatarPic(paciente.tipo)} />
-                    <ListItem.Content>
-                      <ListItem.Title>
-                        <Text>Nombre Paciente: {paciente.nombre}</Text>
-                      </ListItem.Title>
-                      <View>
-                        <Text style={styles.ratingText}>
-                          Nombre Propietario: {paciente.propietario.nombre}
-                        </Text>
-                        <Text style={styles.ratingText}>
-                          Tipo: {paciente.tipo}
-                        </Text>
-                        <Text style={styles.ratingText}>
-                          Raza: {paciente.raza}
-                        </Text>
-                      </View>
-                    </ListItem.Content>
-                    <ListItem.Chevron />
-                  </ListItem.Swipeable>
-                );
-              })}
-            </View>
-          </Box>
-        </ScrollView>
-      )}
-    </NativeBaseProvider>
-  );
+    useFocusEffect(
+        React.useCallback(() => {
+            getPacientes();
+            return () => {
+                setLoading(false);
+                setPacientes([]);
+            };
+        }, [])
+    );
+
+    return (
+        <NativeBaseProvider>
+            {loading ? (
+                <ActivityIndicator
+                    style={styles.indicador}
+                    size="large"
+                    color="rgba(117, 140, 255, 1)"
+                />
+            ) : (
+                <ScrollView>
+                    <Box m={5} mt={8} flex={1} p={1}>
+                        <Heading
+                            size="lg"
+                            color="coolGray.800"
+                            _dark={{
+                                color: "warmGray.50",
+                            }}
+                            fontWeight="semibold"
+                            alignSelf="center"
+                        >
+                            Gestion de Pacientes
+                        </Heading>
+                        <View style={styles.container}>
+                            {pacientes.map((paciente) => {
+                                return (
+                                    <ListItem.Swipeable
+                                        key={paciente.id}
+                                        bottomDivider
+                                        onPress={() => {
+                                            navigation.navigate("TabPaciente", paciente);
+                                        }}
+                                        rightContent={
+                                            <Button
+                                                title="Actualizar"
+                                                onPress={() => {
+                                                    actualizarEmpleado(paciente);
+                                                }}
+                                                icon={{ name: "info", color: "white" }}
+                                                buttonStyle={{ minHeight: "100%" }}
+                                            />
+                                        }
+                                    >
+                                        <Avatar source={avatarPic(paciente.tipo)} />
+                                        <ListItem.Content>
+                                            <ListItem.Title>
+                                                ID: {paciente.id}
+                                            </ListItem.Title>
+                                            <View>
+                                                <Text style={{ paddingTop: 5 }}>Paciente: {paciente.nombre}</Text>
+                                                <Text style={styles.ratingText}>
+                                                    Nombre Propietario: {paciente.propietario.nombre}
+                                                </Text>
+                                                <Text style={styles.ratingText}>
+                                                    Tipo: {paciente.tipo}
+                                                </Text>
+                                                <Text style={styles.ratingText}>
+                                                    Raza: {paciente.raza}
+                                                </Text>
+                                            </View>
+                                        </ListItem.Content>
+                                        <ListItem.Chevron />
+                                    </ListItem.Swipeable>
+                                );
+                            })}
+                        </View>
+                    </Box>
+                </ScrollView>
+            )}
+        </NativeBaseProvider>
+    );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 5,
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: "bold",
-    marginVertical: 30,
-    textAlign: "center",
-  },
-  subtitleView: {
-    flexDirection: "row",
-    paddingLeft: 10,
-    paddingTop: 5,
-  },
-  ratingText: {
-    paddingLeft: 10,
-    color: "grey",
-  },
-  nombrePropietario: {
-    paddingLeft: 10,
-    color: "black",
-  },
-  btn1: {
-    backgroundColor: "orange",
-    padding: 10,
-    margin: 10,
-    width: "95%",
-  },
-  btn2: {
-    backgroundColor: "blue",
-    padding: 10,
-    margin: 10,
-    width: "95%",
-  },
-  btn3: {
-    backgroundColor: "rgb(77, 103, 145)",
-    padding: 10,
-    margin: 10,
-    width: "95%",
-  },
-  pageName: {
-    margin: 10,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center",
-  },
-  btnText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  indicador: {
-    position: "absolute",
-    top: "50%",
-    left: "25%",
-    right: "25%",
-  },
+    container: {
+        flex: 1,
+        padding: 5,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: "bold",
+        marginVertical: 30,
+        textAlign: "center",
+    },
+    subtitleView: {
+        flexDirection: "row",
+        paddingLeft: 10,
+        paddingTop: 5,
+    },
+    ratingText: {
+        paddingLeft: 10,
+        color: "grey",
+    },
+    nombrePropietario: {
+        paddingLeft: 10,
+        color: "black",
+    },
+    btn1: {
+        backgroundColor: "orange",
+        padding: 10,
+        margin: 10,
+        width: "95%",
+    },
+    btn2: {
+        backgroundColor: "blue",
+        padding: 10,
+        margin: 10,
+        width: "95%",
+    },
+    btn3: {
+        backgroundColor: "rgb(77, 103, 145)",
+        padding: 10,
+        margin: 10,
+        width: "95%",
+    },
+    pageName: {
+        margin: 10,
+        fontWeight: "bold",
+        color: "#000",
+        textAlign: "center",
+    },
+    btnText: {
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    indicador: {
+        position: "absolute",
+        top: "50%",
+        left: "25%",
+        right: "25%",
+    },
 });
--- src/views/Paciente/Vacunas.jsx
+++ src/views/Paciente/Vacunas.jsx
@@ -1,40 +1,183 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet as StyleSheetReact } from 'react-native';
 import {
     NativeBaseProvider, VStack, HStack, ZStack, Stack, Center, Box, Heading,
-    Divider, StyleSheet, Text, View, Button, ScrollView, FormControl
-    , InputGroup, InputLeftAddon, WarningOutlineIcon, Input
+    Divider, StyleSheet, Button as ButtonNative, Text, View, ScrollView, FormControl
+    , InputGroup, InputLeftAddon, WarningOutlineIcon, Input, Spacer
 } from 'native-base';
-import * as yup from 'yup';
-import { Formik } from 'formik';
-import { useNavigation } from '@react-navigation/native';
+import { ListItem, Button, Avatar, Badge } from "react-native-elements";
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import {
+    collection,
+    getDocs,
+    query,
+    where,
+    doc,
+    deleteDoc,
+} from "firebase/firestore";
+import { db, auth } from "../../../config/firebase";
+import { ActivityIndicator } from "react-native";
 
-export default function Vacunas({navigation}){
+export default function Vacunas({ navigation, route }) {
+    const { id } = route.params;
+    const [vacunas, setVacunas] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const obtenerVacunasPaciente = async () => {
+        setLoading(true);
+        let listaVacuna = [];
+
+        const querySnapshot = await getDocs(collection(db, "patients", id, "vacunas"));
+        if(querySnapshot.empty) {
+            setLoading(false);
+        } else {
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data())
+                const { dosis, fecha, marca, nombre, peso, proximaDosis, tipo } = doc.data();
+                listaVacuna.push({
+                    id: doc.id,
+                    fecha,
+                    marca,
+                    nombre,
+                    peso,
+                    proximaDosis,
+                    tipo
+                })
+                setLoading(false);
+                setVacunas(listaVacuna);
+                console.log(listaVacuna);
+            });
+        }
+
+    }
+
+    useFocusEffect(
+        React.useCallback(() => {
+            obtenerVacunasPaciente();
+            return () => {
+                setLoading(false);
+                setVacunas([]);
+            };
+        }, [])
+    );
+
+
     return (
-      <NativeBaseProvider>
-                <ScrollView>
-                    <Box flex={1} p={1} w="95%" mx='auto' justifyContent={'center'}>
-                        <VStack  mt={450} space={2} px="2" alignItems="center" rounded='50' justifyContent="center">          
-                            <Stack mb="2.5" mt="1.5" direction={{
-                                base: "column",
-                                md: "row"
-                            }} space={2} mx={{
-                                base: "auto",
-                                md: "0"
+        <NativeBaseProvider>
+            {loading ? (<ActivityIndicator
+                style={styles.indicador}
+                size="large"
+                color="rgba(117, 140, 255, 1)"
+            />) : (<ScrollView>
+                <Box flex={1} p={1} w="95%" mx='auto'>
+                    <VStack mt={15} mb={15} space={2} px="2">
+                        <ButtonNative
+                            backgroundColor={"rgba(117, 140, 255, 1)"}
+                            size="lg" variant="outline" onPress={() => {
+                                navigation.navigate('CreateVacunas');
                             }}>
-                                <Button
-                                backgroundColor= {"rgba(117, 140, 255, 1)"}
-                                size="lg" variant="outline" onPress={() => {
-                                        navigation.navigate('CreateVacunas');
-                                    }}>
-                                   <Text style={{color:'white'}}>  
-                                   Agregar Nueva Vacuna 
-                                   </Text>
-                                </Button>
+                            <Text style={{ color: 'white' }}>
+                                Agregar Nueva Vacuna
+                            </Text>
+                        </ButtonNative>
 
-                            </Stack>
-                        </VStack>
-                    </Box>
-                </ScrollView>
-            </NativeBaseProvider>
+                        <Divider my={4} />
+                        {vacunas.length === 0 ?
+                            <Text>
+                                <Text>El paciente no presenta Vacunas.</Text>
+                                <Text bold>Recuerde que es importante vacunar su mascota</Text>
+                            </Text>
+                            :
+                            vacunas.map((vacuna) => {
+                                return (
+                                    <ListItem.Swipeable
+                                        key={vacuna.id}
+                                        bottomDivider
+                                        onPress={() => {
+                                            console.log("Detalles")
+                                        }}>
+                                        <ListItem.Content>
+                                            <ListItem.Title>
+                                                <Text>Nombre Vacuna: {vacuna.nombre}</Text>
+                                            </ListItem.Title>
+                                            <View>
+                                                <Text style={styles.ratingText}>
+                                                    Marca Vacuna: {vacuna.marca}
+                                                </Text>
+                                                <Text style={styles.ratingText}>
+                                                    Tipo Vacuna: {vacuna.tipo}
+                                                </Text>
+                                            </View>
+                                        </ListItem.Content>
+                                        <ListItem.Chevron />
+                                    </ListItem.Swipeable>
+                                );
+                            })
+                        }
+                    </VStack>
+                </Box>
+            </ScrollView>)}
+
+        </NativeBaseProvider>
     )
 }
+
+const styles = StyleSheetReact.create({
+    container: {
+        flex: 1,
+        padding: 5,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: "bold",
+        marginVertical: 30,
+        textAlign: "center",
+    },
+    subtitleView: {
+        flexDirection: "row",
+        paddingLeft: 10,
+        paddingTop: 5,
+    },
+    ratingText: {
+        paddingLeft: 10,
+        color: "grey",
+    },
+    nombrePropietario: {
+        paddingLeft: 10,
+        color: "black",
+    },
+    btn1: {
+        backgroundColor: "orange",
+        padding: 10,
+        margin: 10,
+        width: "95%",
+    },
+    btn2: {
+        backgroundColor: "blue",
+        padding: 10,
+        margin: 10,
+        width: "95%",
+    },
+    btn3: {
+        backgroundColor: "rgb(77, 103, 145)",
+        padding: 10,
+        margin: 10,
+        width: "95%",
+    },
+    pageName: {
+        margin: 10,
+        fontWeight: "bold",
+        color: "#000",
+        textAlign: "center",
+    },
+    btnText: {
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    indicador: {
+        position: "absolute",
+        top: "50%",
+        left: "25%",
+        right: "25%",
+    },
+});
